aeefe475fa621d08497caa21132f839f0844007e
Tyus Durant <contact@tyusdurant.com>
2019-03-11 16:23:46 -0400

    endof0311

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <div>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, consectetur id enim sed adipisci voluptatibus cumque aperiam laboriosam delectus ullam deleniti minima illum quae praesentium ab rem laborum pariatur blanditiis consequatur molestias sit quidem debitis unde. Autem deleniti asperiores impedit?</p>
+    </div>
+)
+
+
+export default AboutPage
+
+
+
